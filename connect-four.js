ead78c2da97c517400c666e58f03f7d239e1d6c0
sergeveli <70241892+sergeveli@users.noreply.github.com>
2021-05-05 17:37:51 -0400

    Step 2

--- connect-four.js
+++ connect-four.js
@@ -1,16 +1,47 @@
-import Game from './game.js'
+import {Game} from './game.js'
+import {Column} from './game.js'
 
 let game = undefined
  function updateUI(){
+     let targets = document.getElementById('click-targets')
      let gameBoard = document.getElementById('board-holder')
      let gameName = document.getElementById('game-name')
      if(game === undefined){
-         
-         gameBoard.classList.add = 'is-invisible'
+
+         gameBoard.classList.add('is-invisible')
      } else {
-         gameBoard.classList.remove = 'is-invisible'
-         
-         gameName.innerHTML = getName()
+         gameBoard.classList.remove('is-invisible')
+         gameName.innerHTML = game.getName()
+
+
+         for(let rowIndex = 0; rowIndex <= 5; rowIndex++){
+             for(let columnIndex = 0; columnIndex <= 6; columnIndex++){
+                 const square = document.getElementById(`square-${rowIndex}-${columnIndex}`)
+                 square.innerHTML = ''
+                 const playerNumber = game.getTokenAt(rowIndex, columnIndex)
+                 if(playerNumber === 1){
+                     const token = document.createElement('div')
+                     token.classList.add('token')
+                     token.classList.add('black')
+                     square.appendChild(token)
+                 } else if(playerNumber === 2){
+                 const token = document.createElement('div')
+                     token.classList.add('token')
+                     token.classList.add('red')
+                     square.appendChild(token)
+                 }
+                 } 
+                }
+                
+         if(game.currentPlayer === 1){
+             
+             targets.classList.remove('red')
+         targets.classList.add('black')
+         } else if(game.currentPlayer === 2){
+             targets.classList.remove('black')
+             targets.classList.add('red')
+         }
+         gameName.innerHTML = game.getName()
      }
     }
 window.addEventListener('DOMContentLoaded', event => {
@@ -34,6 +65,7 @@ window.addEventListener('DOMContentLoaded', event => {
              newGame.disabled = true
         }
     })
+
     newGame.addEventListener('click', event =>{
         game = new Game(playerOne, playerTwo)
         playerOne.innerHTML = ''
@@ -41,7 +73,16 @@ window.addEventListener('DOMContentLoaded', event => {
         newGame.disabled = true
         updateUI()
     })
-   
+    let targets = document.getElementById('click-targets')
+    targets.addEventListener('click', event =>{
+        const targetID = event.target.id
+        if(!targetID.startsWith('column-')) return 
+        const columnIndex = Number.parseInt(targetID[targetID.length - 1])
+        game.playInColumn(columnIndex)
+        console.log(game.currentPlayer)
+        updateUI()
+    })
+
 })
 
 
--- game.js
+++ game.js
@@ -1,9 +1,53 @@
-export default class Game {
-    constructor(playerOne, playerTwo){
+class Game {
+    constructor(playerOne, playerTwo, currentPlayer){
         this.playerOne = playerOne;
         this.playerTwo = playerTwo;
+        this.currentPlayer = 1
+        this.columns = [
+            new Column(), 
+            new Column(),
+            new Column(),
+            new Column(),
+            new Column(),
+            new Column(),
+            new Column(),
+        ]
     }
     getName(){
-        return `${playerOne} vs. ${playerTwo}`
+        return `${this.playerOne} vs. ${this.playerTwo}`
     }
-}
+    
+
+    getTokenAt(rowIndex, columnIndex){
+        return this.columns[columnIndex].getTokenAt(rowIndex)
+    }
+
+    playInColumn(columnIndex){
+        this.columns[columnIndex].add(this.currentPlayer)
+            if(this.currentPlayer === 1){
+            this.currentPlayer = 2
+        } else if (this.currentPlayer === 2){
+            this.currentPlayer = 1
+        }
+        }
+    }
+
+    class Column {
+        constructor(){
+            this.tokens = [null, null, null, null, null, null];
+        }
+
+        add(currentPlayer){
+            for(let i = 5; i >= 0; i--){
+                if(this.tokens[i] === null){
+                    this.tokens[i] = currentPlayer
+                    break
+                }
+            } 
+        }
+        getTokenAt(rowIndex){
+            return this.tokens[rowIndex]
+        }
+    }
+
+    export{Game, Column}
